5ab1f36ddb9354017e706cb3a05fbd16dec55fdc
JarrenElysanderJuan <jarrenjuan@gmail.com>
2026-01-31 22:12:17 -0500

    Starting Thingys

---
+++ uw-game-jam/Assets/Scripts/AI/Blob.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Blob : MonoBehaviour
+{
+    public bool isTarget = false;
+
+    public void Die()
+    {
+        if (isTarget)
+        {
+            GameManager.instance.Win();
+        }
+        Destroy(gameObject);
+    }
+}
